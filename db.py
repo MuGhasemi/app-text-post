0c795e5c3eb0b03fcc91bbc3a24a6bb060cd805d
mughasemi <mughasemi2008@gmail.com>
2026-08-27 21:32:10 +0330

    create db file, config db setting and create Post table

---
+++ db.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, Column, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    title = Column(String)
+    description = Column(String)
+
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
