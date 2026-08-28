2dc01d8e77ca2128a0fce7284021cfa11af2bb27
mughasemi <mughasemi2008@gmail.com>
2026-08-28 16:23:05 +0330

    Love!

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ app.include_router(router)
 # if __name__ == "__main__":
 #     import uvicorn
 #     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+@app.get("/says")
+def love():
+    return {"muhammad": "I Love You MAEDEW."}
