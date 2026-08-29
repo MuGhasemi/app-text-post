43801bb6dcd33c7f2c747c27e4f054d778a8c1ea
mughasemi <mughasemi2008@gmail.com>
2026-08-29 22:24:40 +0330

    add user router

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
-from services import router
+from services import post_router, user_router
 
 app = FastAPI(
     title="Text Post App",
     docs_url="/documentation",
     redoc_url=None)
 
-app.include_router(router)
+app.include_router(post_router)
+app.include_router(user_router)
+
+
+@app.get("/says")
+def love():
+    return {"muhammad": "I Love You MAEDEW."}
+
 
 ##### for local test ####
 # if __name__ == "__main__":
 #     import uvicorn
 #     uvicorn.run(app, host="127.0.0.1", port=8000)
-
-@app.get("/says")
-def love():
-    return {"muhammad": "I Love You MAEDEW."}
