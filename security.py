f3c949d6401f00d6ace6663bad18aa1082ce02d6
mughasemi <mughasemi2008@gmail.com>
2026-09-04 19:38:42 +0330

    create get_currect_user and add get_user func to this file

--- security.py
+++ security.py
@@ -1,9 +1,14 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
 from pwdlib import PasswordHash
 from datetime import datetime, timedelta, timezone
+from fastapi.security import OAuth2PasswordBearer
+from db import get_db, User
 import jwt
 
 
 pwd_context = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="user/login")
 SECRET_KEY = "J+5Nw+gWUmDUZIbNqDkdKyKqL3A+OWRUCUyl+jH8pGE"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -17,9 +22,32 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
+def get_user(username: str, db: Session = Depends(get_db)) -> User | None:
+    return db.query(User).filter(User.username == username).first()
+
+
 def create_access_token(data: dict, expires_minutes: int = ACCESS_TOKEN_EXPIRE_MINUTES) -> str:
     to_encode: dict = data.copy()
     expire: datetime = datetime.now(
         timezone.utc) + timedelta(minutes=expires_minutes)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str | None = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except jwt.InvalidTokenError:
+        raise credentials_exception
+    user = get_user(username, db)
+    if user is None:
+        raise credentials_exception
+    return user
