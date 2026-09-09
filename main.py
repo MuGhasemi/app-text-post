bcae3ecd2ace3df3198f3dd8ff158e570720deb6
mughasemi <mughasemi2008@gmail.com>
2026-09-09 23:00:05 +0330

    handel CORS Backend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from services import post_router, user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Text Post App",
@@ -8,6 +9,18 @@ app = FastAPI(
 
 app.include_router(post_router)
 app.include_router(user_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "https://app-text-post.onrender.com"
+        ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
 
 
 ##### for local test ####
